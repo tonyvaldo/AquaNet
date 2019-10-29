e0c76327cfd1ac6755cbab6477f82963def6ccf3
Tony <anthony.valdovinos@sjsu.edu>
2019-10-29 15:09:25 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,14 +1,56 @@
+// let checkboxes =[];
+let size = 100;
+let backDropImage;
+let moreFish;
 
-function setup() {
-	// make canvas full screen
-	createCanvas(windowWidth, windowHeight);
+function preload() {
+  backDropImage = loadImage('backdrop.jpg')
+fish[1] = loadImage('fish/fish_1.jpg');
+fish[2] = loadImage('fish/fish_2.jpg');
+fish[3] = loadImage('fish/fish_3.jpg');
+fish[4] = loadImage('fish/fish_4.jpg');
+fish[5] = loadImage('fish/fish_5.jpg');
+fish[6] = loadImage('fish/fish_6.jpg');
+fish[7] = loadImage('fish/fish_7.jpg');
+fish[8] = loadImage('fish/fish_8.jpg');
 
 }
 
+
+function setup() {
+	createCanvas(800, 600);
+	image(backDropImage, 0, 0, width, height);
+	stroke(10);
+	  imageMode(CENTER);
+
+
+	// button = createButton("Press for more fish");
+	// button = select('#moreFish')
+	// button.mousePressed(buttonPressed);
+	// button.class("moreFish")
+
+  moreFish = select('#moreFish')
+  moreFish.mousePressed(pumpMoreFish)
+}
+
+// function myCheckedEvent(){
+// size += 50;
+//
+// }
 
 function draw() {
 	// adding clear() to the draw loop will clear each frame, erasing object trails
 	// clear();
 
-	ellipse(mouseX, mouseY, 100);
+	// ellipse(mouseX, mouseY, size);
+}
+
+function mousePressed() {
+  // for (let i = 1; i < fish.length; i++){
+  //   fish[i].clicked(mouseX, mouseY);
+
+}
+
+function pumpMoreFish(){
+
 }
