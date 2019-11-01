f092a16484b505ed6815e43e70db476db291d8b0
Tony <anthony.valdovinos@sjsu.edu>
2019-10-31 17:40:02 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,45 +1,41 @@
 // let backDropImage;
 // let moreFish;
 let fish = [];
+let fishButton;
 
 //
 function preload() {
   backDropImage = loadImage('backdrop.jpg')
-  fish[1] = loadImage('fish/fish_1.png');
-  fish[2] = loadImage('fish/fish_2.png');
-  fish[3] = loadImage('fish/fish_3.png');
-  fish[4] = loadImage('fish/fish_4.png');
-  fish[5] = loadImage('fish/fish_5.png');
-  fish[6] = loadImage('fish/fish_6.png');
-  fish[7] = loadImage('fish/fish_7.png');
-  fish[8] = loadImage('fish/fish_8.png');
-
+  for (let i = 0; i< 8; i++){
+    fish[i] = loadImage(`fish/fish_${i+1}.PNG`)
+  }
 }
 
 function setup() {
   let cnv = createCanvas(600, 400);
-  cnv.position(100,200);
+  cnv.position(300,180);
+  cnv.parent("#canvasDiv");
+
   image(backDropImage, 15, 25, width, height);
   imageMode(CENTER);
+
+console.log(fish);
+
+fishButton = select("#moreFish");
+fishButton.mousePressed(pumpMoreFish);
 }
 //
 function draw() {
-  // for (let i = 0; i < 9; i++){
-  //   fish[i].move();
-  //   fish[i].show();
 
-//   // adding clear() to the draw loop will clear each frame, erasing object trails
-//   // clear();
-//
-  ellipse(mouseX, mouseY, 25,25);
 }
 //
-// function mousePressed() {
-//   // for (let i = 1; i < fish.length; i++){
-//   //   fish[i].clicked(mouseX, mouseY);
-// }
+function mousePressed() {
+  image(random(fish), mouseX, mouseY, random(width/4, height/4),random(width/4, height/4));
+  console.log("fish");
+
+}
 //
 //
-// function pumpMoreFish() {
-//
-// }
+function pumpMoreFish() {
+
+}
